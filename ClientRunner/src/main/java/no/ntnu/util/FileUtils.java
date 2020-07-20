f89bdb1472c2689e8abd7ab870bfd51302d1d10f
trygve <trygve.woldseth@gmail.com>
2020-07-20 15:58:52 +0200

    added file utils

---
+++ ClientRunner/src/main/java/no/ntnu/util/FileUtils.java
@@ -0,0 +1,33 @@
+package no.ntnu.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileUtils {
+
+    public static final File executionDir = new File("").getAbsoluteFile();
+
+    /**
+     * Cheks whther or not the given file is a child of the given directory.
+     * if ether is null false is returned
+     * @param file The file to check if is in the directory
+     * @param parent The dir to check if the file is in
+     * @return true if the file is in any of the dirs sub directorys
+     */
+    public static boolean isFileChildOfDir(File file, File parent){
+        boolean isChild = false;
+        try{
+            if (file.exists() && parent.exists()){
+                if (file.getCanonicalPath().startsWith(parent.getCanonicalPath())){
+                     isChild = true;
+                }
+            }
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return isChild;
+    }
+}
